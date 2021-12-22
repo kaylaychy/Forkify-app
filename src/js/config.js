dad8797b86df0d9b0d4f8f4433e2305f6fe96b6c
kaylaychy <kelechiijeomah@gmail.com>
2021-12-22 11:00:50 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6b64886e-707c-4868-878e-7b2b4e66a512';
+export const MODAL_CLOSE_SEC = 2.5;
